42663e4de8fe6f94f305f66b0fa20470eb25e422
daragh <daragh.hamilton8@gmail.com>
2026-05-01 00:18:27 +0100

    layout change

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
-fetch('/header.html')
+fetch('/com.header.html')
   .then(res => res.text())
   .then(data => {
     document.getElementById('header').innerHTML = data;
+  });
+
+fetch('/com.sidebar.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('sidebar').innerHTML = data;
+  });
+
+fetch('/com.sidebar.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('sidebar').innerHTML = data;
   });
